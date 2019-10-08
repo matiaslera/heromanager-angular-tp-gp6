5710f0d78d6b589b0afc6ead0b71059b006ba18c
javiemgz <javiemgz@gmail.com>
2019-10-08 18:30:29 -0300

    arreglado navegacion al login si no esta log

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree ,Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/loginService/login.service';
 
@@ -7,11 +7,17 @@ import { LoginService } from '../services/loginService/login.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private userLogService: LoginService){}
+  constructor(private userLogService: LoginService, private router:Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.userLogService.isAuthenticated()
+    if(!this.userLogService.isAuthenticated()){
+      this.router.navigate(['/'])
+      false
+    } 
+    else{
+      true
+    }
   }
 
 }
