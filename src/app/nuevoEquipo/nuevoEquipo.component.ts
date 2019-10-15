154397781da2ca76fa8905707649d07dc57c6d02
matiaslera <matiasmal94@gmail.com>
2019-10-14 23:26:52 -0300

    nuevoEquipo
    Se agrega la lista de equipo

--- src/app/nuevoEquipo/nuevoEquipo.component.ts
+++ src/app/nuevoEquipo/nuevoEquipo.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IndividuoService } from 'src/app/services/individuos/Individuos.service';
 @Component({
-  selector: 'app-nuevoEquipo',
+  selector: 'app-nuevoEquipo' ,
   templateUrl: './nuevoEquipo.component.html',
   styleUrls: ['./nuevoEquipo.component.css']
 })
 export class NewEquipoComponent implements OnInit {
 
+ individuos = individuos;
+ 
   constructor() { }
 
   ngOnInit() {
+   
   }
 
-  lider(){
-    
-  }
+
 }
+export const individuos = [
+  {
+    name: ' Hulk',
+    description: ' Es verde'
+  },
+  {
+    name: 'Superman',
+    description: 'Sabe volar'
+  },
+  {
+    name: 'Spider-Man',
+    description: 'Le gusta las arañas'
+  }
+];
+export class Hero {
+  id: number;
+  name: string;
+}
+
---
+++ src/app/services/individuos/Individuos.service.ts
@@ -0,0 +1,11 @@
+
+import { NewEquipoComponent } from 'src/app/nuevoEquipo/nuevoEquipo.component';
+import { MessageService } from 'src/app/services/individuos/mensajes.service';
+
+
+export class IndividuoService {
+  
+}
+
+
+  
