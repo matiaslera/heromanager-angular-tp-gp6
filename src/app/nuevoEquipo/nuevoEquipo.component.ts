b644e205063021e720e0e2b9e3434f34f20f2e84
matiaslera <matiasmal94@gmail.com>
2019-10-25 19:51:22 -0300

    nuevoComponent-html

--- src/app/nuevoEquipo/nuevoEquipo.component.ts
+++ src/app/nuevoEquipo/nuevoEquipo.component.ts
@@ -1,71 +1,89 @@
 import { Component, Inject, Optional, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import {Usuario} from '../domain/usuario';
-import {Equipo} from '../domain/misequipos'
-import {EquiposService} from '../services/individuos/misequipos.service' 
+import { FormControl } from '@angular/forms';
+import { Usuario } from '../domain/usuario';
+import { Equipo } from '../domain/misequipos'
+import { EquiposService } from '../services/individuos/misequipos.service'
 import { Router } from '@angular/router';
-import {MisEquiposComponent} from '../misEquipos/misEquipos.component'
-  
-export interface UsersData {
-  name: string;
-  id: number;
-}
+import { UsuariosService } from '../services/individuos/usuariosService'
+import{FriendsService} from '../services/typeRelationService/friendsService/friends.service'
+import { Individuo } from 'src/app/domain/Individuo';
+import {HEROES} from '../services/individuos/Individuos.service'
+import { LoginService } from '../services/loginService/login.service';
+
 @Component({
-  selector: 'app-nuevoEquipo' ,
+  selector: 'app-nuevoEquipo',
   templateUrl: './nuevoEquipo.component.html',
   styleUrls: ['./nuevoEquipo.component.css']
 })
 export class NewEquipoComponent implements OnInit {
+  
+  nombreEquipo:string;
+  liderEquipo:Usuario;
+  integrantes= [];
+  propietario = this.getUser;
 
-  equipo:Equipo;
-  misEquipo:MisEquiposComponent;
- individuos = individuos;
- action:string;
- local_data:any;
+  equipo: Equipo;
+  action: string;
+  local_data: any;
 
- constructor(private equipoService:EquiposService, private router:Router, 
-   public dialogRef: MatDialogRef<NewEquipoComponent>,
-   @Optional() @Inject(MAT_DIALOG_DATA) public data: EquiposService) {
-   console.log(data);
-   this.local_data = {...data};
-   this.action = this.local_data.action;
-   if(!this.equipo){
-     this.navegarHome()
-   }
-    }
+  miFormulario = new FormControl();
+  seleccionado: Usuario;
+  
 
- doAction(){
-   this.dialogRef.close({event:this.action,data:this.local_data});
- }
+  constructor(private equipoService: EquiposService, private router: Router,private integranteFri: FriendsService,
+    public dialogRef: MatDialogRef<NewEquipoComponent>, private loginService: LoginService,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: Equipo) {
 
- closeDialog(){
-   this.dialogRef.close({event:'Cancel'});
- }
+    console.log(data);
+    this.local_data = { ...data };
+    this.action = this.local_data.action;
+      }
+      ngOnInit() {
 
-  ngOnInit() {
-   //this.equipo.propietario = new Usuario(this.misEquipo.perfil) 
-  }
+      }
 
-  mensajePrueba(){
+  mensajePrueba() {
     window.alert('El componente esta funcionando');
   }
-  
-  navegarHome(){
+
+  doAction() {
+    this.dialogRef.close({ event: this.action, data: this.local_data });
+  }
+
+  closeDialog() {
+    this.dialogRef.close({ event: 'Cancel' });
+  }
+
+  navegarHome() {
     this.router.navigate(['/misequipos'])
   }
 
-}
-export const individuos = [
-  {
-    name: ' Hulk',
-    description: ' Es verde'
-  },
-  {
-    name: 'Superman',
-    description: 'Sabe volar'
-  },
-  {
-    name: 'Spider-Man',
-    description: 'Le gusta las arañas'
+  seleccion(user:Usuario){
+    this.seleccionado = user;
   }
-];
+  agregarIntegrante(name:string){
+    this.integrantes.push(new Usuario(name));
+      
+  }
+
+   esUnIntegrante(usuario:Usuario){
+     return this.integrantes.includes(usuario)
+ }
+ eliminarIntegrante(usuario:Usuario){
+     if (this.esUnIntegrante(usuario)) {
+       return this.integrantes.splice(this.integrantes.indexOf(usuario), 1)
+   }
+   }
+   getUser() {
+    return this.loginService.getUser()
+  }
+
+  crearEquipo(){
+    this.equipo.nombre= this.nombreEquipo
+    this.equipo.integrantes= this.integrantes
+    this.equipo.propietario=this.propietario()
+    this.equipo.lider = this.liderEquipo
+  }
+
+}
