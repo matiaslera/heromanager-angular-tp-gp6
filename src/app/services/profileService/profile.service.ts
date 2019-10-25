de3f9e7749932cb8052a392b17d69943ac9f8245
javiemgz <javiemgz@gmail.com>
2019-10-25 00:15:21 -0300

    solved profile problem

--- src/app/services/profileService/profile.service.ts
+++ src/app/services/profileService/profile.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { LoginService } from '../loginService/login.service';
 import { Individuo } from 'src/app/domain/Individuo';
 import { REST_SERVER_URL } from '../configuration';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,7 @@ export class ProfileService {
 
   constructor(private httpCLient: HttpClient, private loginService: LoginService) { }
 
-  userLogged() {
-    return this.loginService.getUser()
-  }
   async getFullProfile(id:String ) {
-    return await this.httpCLient.post<Individuo>(REST_SERVER_URL + '/perfil_completo',this.userLogged).toPromise()
+    return of(await this.httpCLient.get<Individuo>(REST_SERVER_URL + '/perfil_completo/' +id).toPromise())
   }
 }
